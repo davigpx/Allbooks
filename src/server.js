f89d481eca0709f2174d01fecbfbd85983d99933
Davi Gurgel <davigp500@gmail.com>
2025-12-11 14:30:10 -0300

    Change server port from 8000 to 8001

--- src/server.js
+++ src/server.js
@@ -18,6 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("Servidor iniciado na porta 8000")
+  console.log("Servidor iniciado na porta 8001")
   console.log("API disponível em http://localhost:8000")
 })
